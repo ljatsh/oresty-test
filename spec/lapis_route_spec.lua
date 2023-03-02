28069debdd2e8fb4628046940921b8abf72ebd9f
lj@sh <longjun.wang@qq.com>
2023-03-02 13:56:59 +0800

    backup after termination

--- spec/lapis_route_spec.lua
+++ spec/lapis_route_spec.lua
@@ -2,18 +2,64 @@
 local lapis = require('lapis.application')
 local mock_request = require('lapis.spec.request').mock_request
 local use_test_env = require('lapis.spec').use_test_env
+local respond_to = require("lapis.application").respond_to
 
 local app = lapis.Application()
 
-app:match('/hello', function(self)
-  return "welcome to my page"
+app:match('/page/:id[%d]/:name', function(self)
+  print("welcome to my page")
+
+  for k, v in pairs(self.params) do
+    print(k, '=', v)
+  end
+end)
+app:match('/hello/lj', function(self)
+  print('/hello/lj')
+end)
+app:match('/browse/*', function(self)
+  print(self.params.splat)
+  for k, v in pairs(self.params) do
+    print(k, '=', v)
+  end
+end)
+app:match('/settings(/:username(/:page))(.:format)', function(self)
+  print(self.params.splat)
+  for k, v in pairs(self.params) do
+    print(k, '=', v)
+  end
 end)
 
-describe('lapis route #lapis_route', function()
-  it('should make a request', function()
-    local status, body = mock_request(app, '/hello')
+app:match('edit_user', '/edit_user/:id[%d]', respond_to({
+  before = function(self)
+    print(debug.traceback('before', 3))
+  end,
 
-    assert.are.same(ngx.HTTP_OK, status)
-    assert.is.truthy(body:match('welcome to my page'))
+  GET = function(self)
+    print(debug.traceback('GET', 3))
+  end,
+
+  POST = function(self)
+    print(debug.traceback('POST', 3))
+  end
+}))
+
+function app.handle_404(self)
+  print('..... 4-4')
+  print(self.params.splat)
+  for k, v in pairs(self.params) do
+    print(k, '=', v)
+  end
+end
+
+describe('lapis route #lapis_route', function()
+  -- it('should make a request', function()
+  --   local status, body = mock_request(app, '/hello')
+
+  --   assert.are.same(ngx.HTTP_OK, status)
+  --   assert.is.truthy(body:match('welcome to my page'))
+  -- end)
+
+  it('test', function()
+    mock_request(app, '/edit_user/11')
   end)
 end)
---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env luajit
+
+-- Busted command-line runner
+require 'busted.runner'({ standalone = false })
